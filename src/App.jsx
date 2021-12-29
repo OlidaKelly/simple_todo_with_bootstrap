b6cb490f0b4f04e6e37d302b71b630514201a474
kelly <kelly.olida.pro@gmail.com>
2021-12-29 12:39:22 +0100

    webApp cleaned and Bootstrap installed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <div className="App">
+     <h1>Hello, on my to do list !</h1>
+     <button class="btn btn-danger">Add To Do</button>
+    </div>
+  );
+}
+
+export default App;
